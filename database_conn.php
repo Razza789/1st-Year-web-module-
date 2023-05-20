d973c8a082484e22db4bb04091a836ccc63760e8
Ryan <34662336+Razza789@users.noreply.github.com>
2023-05-20 13:14:09 +0100

    Update database_conn.php

--- database_conn.php
+++ database_conn.php
@@ -1,5 +1,5 @@
 <?php
-   $dbConn = new mysqli('localhost', '', '!', ''); //connects to the newnumyspace
+   $dbConn = new mysqli('localhost', '', '', ''); //connects to the newnumyspace
 
    if ($dbConn->connect_error) {
       echo "<p>Connection failed: ".$dbConn->connect_error."</p>\n";
